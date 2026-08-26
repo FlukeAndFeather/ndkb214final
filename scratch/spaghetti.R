8a0197a082ebb291d859441c5ad59415d8fb977b
Nina Berry <ninadkberry@gmail.com>
2026-08-26 14:37:16 -0700

    add spaghetti

--- scratch/spaghetti.R
+++ scratch/spaghetti.R
@@ -1,10 +1,11 @@
 library(tidyverse)
-qc_1_draft <- read_csv("data/QuebradaCuenca1-Bisley.csv")
+qc1_draft <- read_csv("data/QuebradaCuenca1-Bisley.csv")
 
-# Make a blank data table 1988-1994
-# Get rid of colums I do not need: only want "date" and "Ca"
+qc1_filtered <- qc1_draft |> 
+  select("Sample_Date", "Ca") |> 
+  filter(Sample_Date >= "1988-01-01" & Sample_Date <= "1994-12-31")
 
-qc_1_filtered_spaghetti <- qc_1_draft |> 
+qc_1_filtered_spaghetti <- qc1_draft |> 
   select("Sample_Date", "Ca") |> 
   filter(Sample_Date >= "1988-01-01" & Sample_Date <= "1994-12-31")
 
@@ -20,4 +21,5 @@ qc_1_Ca <- qc_1_filtered_spaghetti |>
   geom_line()
 
 qc_1_Ca
+
 
